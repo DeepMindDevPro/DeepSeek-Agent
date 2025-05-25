cf22662dfeb6d8ac7883537cc43b0e8d831b491d
gechunfa1 <gechunfa1@jd.com>
2025-05-25 09:36:21 +0800

    fix ChatWindow.js

---
+++ deepseek_web/src/webpack.config.js
@@ -0,0 +1,15 @@
+//// webpack.config.js
+//module.exports = {
+//  module: {
+//    rules: [
+//      {
+//        test: /\.tsx?$/,
+//        use: 'ts-loader',
+//        exclude: /node_modules/,
+//      },
+//    ],
+//  },
+//  resolve: {
+//    extensions: ['.ts', '.tsx', '.js', '.jsx'], // 添加对 ts/tsx 的解析
+//  },
+//};
