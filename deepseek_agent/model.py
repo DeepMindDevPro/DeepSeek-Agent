e491abcc569e2dbe85ef87a1a5e523832a956dec
gechunfa1 <gechunfa1@jd.com>
2025-06-05 09:09:28 +0800

    bug fix

--- deepseek_agent/model.py
+++ deepseek_agent/model.py
@@ -33,7 +33,7 @@ class DeepSeekChatModel:
             stream: bool = False,
             extra_generate_cfg: Optional[Dict] = None,
     ) -> Generator[dict, None, None]:
-        # TODO
+        # TODO 支持大模型以及小模型的组合作业
         # 根据任务类型选择模型（示例: 复杂任务用大模型，简单任务用轻量模型）
         headers = {}
         request_data = {"model": self.model_name, "messages": messages, "stream": False}  # 强制设置stream=False
